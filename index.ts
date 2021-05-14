b00fec0d12a1467133fbae2d66b23c8db63219b1
Giovane2000 <giovane10.seabra@gmail.com>
2021-05-13 21:59:15 -0400

    titulo commit 03
    atauliazando programa -classes

--- index.ts
+++ index.ts
@@ -1,4 +1,33 @@
-let variavel: string; 
+class jogador_futebol{
 
-console.log('teste 021');
-console.log('ok,ok 8969');
+    nome: string;
+    idade: number;
+    altura: number;
+    peso: number
+    habilidade: number;
+    impulsao: number;
+    passe: number;
+    forca: number;
+    chute: number;
+    cabeceio: number;
+    
+    constructor(nome: string, idade:number, altura: number, peso: number, habilidade: number, impulsao: number, passe:number, forca: number, chute: number, cabeceio: number){
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.habilidade = habilidade;
+        this.impulsao = impulsao;
+        this.passe = passe;
+        this.forca = forca;
+        this.chute = chute;
+        this.cabeceio = cabeceio;
+
+    }
+
+
+}
+
+let giovane: jogador_futebol = new jogador_futebol('giovane', 20, 1.80, 69, 70, 91, 75, 56, 88, 82);
+
+console.log(giovane); 
